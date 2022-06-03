00a261ee2dd4037fdf14d9b27f6aabb2bf741eb7
Irfan Maulana Akbar <irfanjamesway@yahoo.co.id>
2022-06-03 15:01:49 +0700

    Create testing scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,9 +9,14 @@ module.exports = {
  networks: {
    ropsten: {
      url: "https://ropsten.infura.io/v3/439b4ade591f4c0480738ea457ccf22d",
-     accounts: [process.env.PRIVATE_KEY],
+     accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_1, process.env.PRIVATE_KEY_2],
      gasPrice: 50000000000
    },
+   shibuya: {
+     url: "https://evm.shibuya.astar.network",
+     accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_1, process.env.PRIVATE_KEY_2],
+     gasPrice: 30000000000
+   }
  },
  etherscan: {
    apiKey: process.env.ETHERSCAN_API_KEY,
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,51 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai')
+const { BigNumber } = require('ethers')
+const { ethers, upgrades } = require('hardhat')
+// const { contractAddress, contractAbi } = require("../config")
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe('Staking', function () {
+  var NFTAddress
+  var NFTentity
+  var marketplace
+  const platformAddress = "0xEE349714fBf674DEBC335561C86aE47ddE743F5A"
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  it('Deploy NFT', async function () {
+    const NFTDeployer = await ethers.getContractFactory("NFTFish")
+    console.log("Deploying NFT...")
+    NFTentity = await NFTDeployer.deploy()
+    await NFTentity.deployed()
+    NFTAddress = NFTentity.address
+    console.log("NFT Address deployed to: ", NFTAddress)
+  })
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  it('Deploy Marketplace', async function () {
+    const Marketplace = await ethers.getContractFactory("Marketplace");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+    console.log("Deploying Marketplace...");
+   
+    marketplace = await upgrades.deployProxy(Marketplace, [NFTAddress, platformAddress], {
+      initializer: "initialize",
+    });
+    await marketplace.deployed();
+   
+    console.log("Marketplace deployed to:", marketplace.address);
+  })
+  it('Mint Token NFT', async function () {
+    const MintNFT = await NFTentity.mint("0xEE349714fBf674DEBC335561C86aE47ddE743F5A",1, "https://")
+    await MintNFT.wait()
+    const equalto = await NFTentity.ownerOf(1)
+    expect(equalto.toString()).to.equal("0xEE349714fBf674DEBC335561C86aE47ddE743F5A")
+  })
+  it('Approve NFT', async function () {
+    const ApproveNFT = await NFTentity.approve(marketplace.address, 1)
+    await ApproveNFT.wait()
+    expect(ApproveNFT.hash).to.not.null
+  })
+  it('Buy NFT', async function () {
+    const [addr1, addr2, addr3] = await ethers.getSigners()
+    
+    const BuyNFT = await marketplace.connect(addr2).buy(addr1.address, 1, {value: ethers.utils.parseEther("1")})
+    await BuyNFT.wait()
+    expect(BuyNFT.hash).to.not.null
+  })
+})
